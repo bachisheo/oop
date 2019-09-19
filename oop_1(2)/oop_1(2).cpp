a34970b9ea8cf6ed149a8af51d1061ee67c8f4ae
Olga Bachishche <terenrey@gmail.com>
2019-09-19 20:50:10 +0700

    second example

---
+++ oop_1(2)/oop_1(2).cpp
@@ -0,0 +1,56 @@
+﻿// oop_1(2).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+typedef struct apples {
+	int count = 1;                     //количество
+	int weight = 1;                    //вес
+};
+void Init(apples &appl, int a, int b) {                 //инициализация
+	appl.count = a;
+	appl.weight = b;
+	printf("\nФрукты инициализированы.");
+}
+void Read(apples &appl) {                //ввод с клавиатуры
+	printf("\nВведите количество яблок: ");
+	scanf_s("%d", &appl.count);
+	printf("\nВведите вес яблок: ");
+	scanf_s("%d", &appl.weight);
+}
+void Display(apples appl) {             //вывод на экран
+	printf ("\\");
+	for (int i = appl.count; i > 0; i--)
+		//printf("Ѽ");
+		printf("() ");
+	printf("/");
+}
+
+apples Add(apples a, apples b) {				 //сложение дробей без сокращения
+	apples summ;
+	summ.weight = a.weight + b.weight;
+	summ.count = a.count + b.count;
+	return summ;
+}
+float middle_w(apples appl) {                      //метод нахождения среднего веса яблок
+
+	return 1.0 * appl.weight/ appl.count;
+}					                            
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	apples one, two, sum;
+	Init(one, 8, 65);
+	Display(one);
+	printf("\nВведите новую порцию яблок!");
+	Read(two);
+	printf("\nВаша порция:");
+	Display(two);
+	sum = Add(one, two);
+	printf("Их сумма:");
+	Display(sum);
+	printf("Давайте найдем средний вес яблока! Он равен %g кг", middle_w(sum));
+}
--- oop_1/oop_1.cpp
+++ oop_1/oop_1.cpp
@@ -3,23 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-typedef struct fraction {
-	int num = 1;                     //числитель
-	int denom = 1;                   //знаменатель
+typedef struct apples {
+	int count = 1;                     //числитель
+	int weight = 1;                   //знаменатель
 };
-void Init(fraction &fract, int a, int b) {                 //инициализация
-	fract.num = a;
-	fract.denom = b;
+void Init(apples &fract, int a, int b) {                 //инициализация
+	fract.count = a;
+	fract.weight = b;
 	printf("\nДробь инициализированна.");
 }
-void Read(fraction &fract) {                //ввод с клавиатуры
+void Read(apples &fract) {                //ввод с клавиатуры
 	printf("\nВведите числитель: ");
-	scanf_s("%d", &fract.num);
+	scanf_s("%d", &fract.count);
 	printf("\nВведите знаменатель: ");
-	scanf_s("%d", &fract.denom);
+	scanf_s("%d", &fract.weight);
 }
-void Display(fraction fract) {             //вывод на экран
-	printf("\n   %d\n   - \n   %d\n", fract.num, fract.denom);
+void Display(apples fract) {             //вывод на экран
+	printf("\n   %d\n   - \n   %d\n", fract.count, fract.weight);
 }
 int nod(int a, int b) {                    //нахождение наименьшего общего кратного через нод
 	while (a != 0 && b != 0) {             //нахождение наибольшего общего делимого через алгоритм Евклида
@@ -29,15 +29,15 @@ int nod(int a, int b) {                    //нахождение наимень
 	}
 	return a + b;
 }
-fraction Add(fraction a, fraction b) {				 //сложение дробей без сокращения
-	fraction summ;
-	summ.denom = (a.denom == b.denom ? a.denom: a.denom * b.denom/nod(a.denom, b.denom));
-	summ.num = (a.num * b.denom + b.num * a.denom) /nod(a.denom, b.denom);
+apples Add(apples a, apples b) {				 //сложение дробей без сокращения
+	apples summ;
+	summ.weight = (a.weight == b.weight ? a.weight: a.weight * b.weight/nod(a.weight, b.weight));
+	summ.count = (a.count * b.weight + b.count * a.weight) /nod(a.weight, b.weight);
 	return summ;
 }
-float separator(fraction fract) {                      //метод выделения дробной части
-	float ost = fract.num > fract.denom ? fract.num % fract.denom : fract.num;
-	ost /= 1.0 * fract.denom;
+float middle_w(apples fract) {                      //метод выделения дробной части
+	float ost = fract.count > fract.weight ? fract.count % fract.weight : fract.count;
+	ost /= 1.0 * fract.weight;
 	return ost;
 }					                            
 
@@ -45,7 +45,7 @@ float separator(fraction fract) {                      //метод выделе
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	fraction one, two, sum;
+	apples one, two, sum;
 	Init(one, 14, 7);
 	Display(one);
 	printf("\nВведите новую дробь!");
@@ -56,7 +56,7 @@ int main()
 	printf("Их сумма:");
 	Display(sum);
 	printf("Давайте найдем ее дробную часть! Она равна ");
-	printf("%g", separator(sum));
+	printf("%g", middle_w(sum));
 	
 }
 
